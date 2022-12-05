40e10df2daf6d6fcd836c9c93283e834ddd9abfa
savvsnowflake <119891129+savvsnowflake@users.noreply.github.com>
2022-12-06 00:59:34 +0530

    Update streamlit_app.py
    added breakfast menu item

--- streamlit_app.py
+++ streamlit_app.py
@@ -1,4 +1,5 @@
 import streamlit
-streamlit.title('My parents helathy diner')
-streamlit .header('omega 3')
-streamlit.text('calcium')
+streamlit.header('Breakfast Menu')
+streamlit.text('Omega 3 & Blueberry Oatmeal')
+streamlit.text('Kale, Spinach & Rocket Smoothie')
+streamlit.text('Hard-Boiled Free-Range Egg')
