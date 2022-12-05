0e754b73063a19f50a40fd2d21e098239fd19e9e
savvsnowflake <119891129+savvsnowflake@users.noreply.github.com>
2022-12-06 01:01:18 +0530

    Update streamlit_app.py

--- streamlit_app.py
+++ streamlit_app.py
@@ -1,5 +1,5 @@
 import streamlit
-streamlit.header(🥣'Breakfast Menu')   
-streamlit.text(🥗'Omega 3 & Blueberry Oatmeal')
-streamlit.text(🐔'Kale, Spinach & Rocket Smoothie')
-streamlit.text(🥑'Hard-Boiled Free-Range Egg'🍞)
+streamlit.header('🥣Breakfast Menu')   
+streamlit.text('🥗Omega 3 & Blueberry Oatmeal')
+streamlit.text('🐔Kale, Spinach & Rocket Smoothie')
+streamlit.text('🥑Hard-Boiled Free-Range Egg'🍞)
